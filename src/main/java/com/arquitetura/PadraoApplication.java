63d1a6de01ce5da2394f30a78356afb705b53de3
ivonildo-lopes <ivonildo-lopes@gmail.com>
2018-01-31 15:49:10 -0300

    subindo o projeto webservice rest modelo

---
+++ src/main/java/com/arquitetura/PadraoApplication.java
@@ -0,0 +1,12 @@
+package com.arquitetura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadraoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadraoApplication.class, args);
+	}
+}
